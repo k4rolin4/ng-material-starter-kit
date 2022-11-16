9ccc80ececa3e941c3f7dd0371e352f36695b1ad
k4olin4 <karolina.kozinska16@gmail.com>
2022-11-16 01:21:21 +0100

    lists: exercise 2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ProductFormComponent } from './components/product-form/product-form.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
+import { CategoriesListComponent } from './components/categories-list/categories-list.component';
 import { ProductFormComponentModule } from './components/product-form/product-form.component-module';
 import { ProductServiceModule } from './services/product.service-module';
 import { ProductListComponentModule } from './components/product-list/product-list.component-module';
 import { ProductsServiceModule } from './services/products.service-module';
+import { CategoriesListComponentModule } from './components/categories-list/categories-list.component-module';
+import { CategoriesServiceModule } from './services/categories.service-module';
 
 @NgModule({
-  imports: [RouterModule.forRoot([{ path: 'create-product', component: ProductFormComponent }, { path: 'products', component: ProductListComponent }]), ProductFormComponentModule, ProductServiceModule, ProductListComponentModule, ProductsServiceModule],
+  imports: [RouterModule.forRoot([{ path: 'create-product', component: ProductFormComponent }, { path: 'products', component: ProductListComponent }, { path: 'categories', component: CategoriesListComponent }]), ProductFormComponentModule, ProductServiceModule, ProductListComponentModule, ProductsServiceModule, CategoriesListComponentModule, CategoriesServiceModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule {
---
+++ src/app/components/categories-list/categories-list.component-module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CategoriesListComponent } from './categories-list.component';
+import {MatCardModule} from "@angular/material/card";
+import {MatListModule} from "@angular/material/list";
+
+@NgModule({
+  imports: [CommonModule, MatCardModule, MatListModule],
+  declarations: [CategoriesListComponent],
+  providers: [],
+  exports: [CategoriesListComponent]
+})
+export class CategoriesListComponentModule {
+}
